3dce1eacf859d776393bbc70f82228285d157348
Paolo Flores <pflores.fisi22@gmail.com>
2023-08-26 22:36:12 -0500

    ci: switch deploy to production branch

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://paoloose.github.io',
+  site: 'https://paoloose.site',
   base: '/discrete-mathematics'
 });
