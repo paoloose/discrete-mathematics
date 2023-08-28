a64f9aad28a5840fad19d0b154768112943c662e
Paolo Flores <pflores.fisi22@gmail.com>
2023-08-28 10:45:36 -0500

    feat: log panic errors with `console_error_panic_hook`

--- assignments/week1/logic-parser-wasm/src/lib.rs
+++ assignments/week1/logic-parser-wasm/src/lib.rs
@@ -1,20 +1,22 @@
+use std::panic;
 use wasm_bindgen::prelude::*;
 
 use logic_parser::lexer::Lexer;
 use logic_parser::parser::Parser;
 
 #[wasm_bindgen]
-extern "C" {
+extern {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn parse_expression(expr: &str) -> String {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
     // "p & q" -> [ & [p] [q]]
     // "p & (q => s)" -> [ & [p] [ => [q] [s]]]
     // "(p | q) & (q => s)" -> [ | [p] [q] ]
     let tokens = Lexer::new(expr).parse().unwrap();
     let ast = Parser::new(&tokens).parse().unwrap();
 
-    format!("{ast}", ast=ast.as_str())
+    format!("{ast}", ast=ast.as_json())
 }
