b7887013a4a7d1328c5fbe4d0b1a1e256e79ec9e
Paolo Flores <pflores.fisi22@gmail.com>
2023-08-31 19:02:38 -0500

    setup typescript and fix routes

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://paoloose.site',
-  base: '/discrete-mathematics'
+  base: '/discrete-mathematics',
+  output: 'static'
 });
