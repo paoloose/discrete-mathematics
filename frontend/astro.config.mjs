8c0dab4c1c350842e2459384b0adc6222defb505
Paolo Flores <pflores.fisi22@gmail.com>
2023-08-31 19:13:45 -0500

    add support for react

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://paoloose.site',
   base: '/discrete-mathematics',
-  output: 'static'
+  output: 'static',
+  integrations: [react()]
 });
