41e4e589b406cb3c8a1862b1cca5e593181453c6
Paolo Flores <pflores.fisi22@gmail.com>
2023-09-06 06:44:08 -0500

    fix tree drawing

--- assignments/week1/logic-parser-wasm/web_preview.sh
+++ assignments/week1/logic-parser-wasm/web_preview.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-wasm-pack build --target web -d ./www/pkg && \
-    python3 -m http.server -d ./www
+www_dir="./www"
+serve_command="python3 -m http.server -d $www_dir"
+
+if which live-server > /dev/null 2>&1; then
+    serve_command="live-server $www_dir"
+fi
+
+wasm-pack build --target web -d $www_dir/pkg && $serve_command
