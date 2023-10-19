e94b709fa0e1cbf5bcdea91d0a25dd68eb343cbb
Paolo Flores <pflores.fisi22@gmail.com>
2023-10-19 14:03:49 -0500

    feat: handle CORS

--- assignments/week2/server/vennbase/settings.py
+++ assignments/week2/server/vennbase/settings.py
@@ -33,6 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -44,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    # from dependencies
+    'corsheaders',
+    # from project apps
     'api',
     'records'
 ]
@@ -51,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
